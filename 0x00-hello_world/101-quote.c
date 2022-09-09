6dfd63b7452018b0abb0bcaeb24a389b7b4eaaf0
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-08 17:28:20 -0700

    enocre

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59, sizeof(char), stderr);
-	return (0);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, sizeof(char), stderr);
+	return (1);
 }

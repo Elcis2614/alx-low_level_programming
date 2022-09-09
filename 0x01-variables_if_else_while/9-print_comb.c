bc4d25a6c0c4fb865caa4ca3d876948c6b09c503
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-09 12:47:23 -0700

    displaying with comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	/*print digits*/
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		putchar(44);
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}

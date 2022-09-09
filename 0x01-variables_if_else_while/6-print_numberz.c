08ecd9d87b27db0eeea0396bdd0308e03d509584
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-09 12:00:49 -0700

    display digit with getchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	/*print digits*/
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}

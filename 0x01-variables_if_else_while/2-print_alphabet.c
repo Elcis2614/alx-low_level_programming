47ef6bf44dd31a7d39b64e3280f6a28a6d2399c0
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-09 11:12:04 -0700

    printing using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

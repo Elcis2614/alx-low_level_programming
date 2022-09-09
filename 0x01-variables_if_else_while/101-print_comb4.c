c6ae6f147ae0f36c7e7e2f697f31e8f52578541f
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-09 13:59:47 -0700

    3 digit combination possibilities

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	/*print digits*/
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = i + 1; j <= 57; j++)
+		{
+			for (k = j + 1; k <= 57; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i != 55)
+				{
+					putchar(44);
+					putchar(' ');
+				}
+			}
+	
+		}
+	}
+	putchar('\n');
+	return (0);
+}

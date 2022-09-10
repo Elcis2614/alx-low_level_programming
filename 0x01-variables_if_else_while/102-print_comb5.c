493b51239bcce770d011d33b73a25231b8a84e33
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-09 17:16:15 -0700

    crazy iterations

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,7 +15,7 @@ int main(void)
 	/*print digits*/
 	for (i = 48; i <= 57; i++)
 	{
-		for (j = 48; j <= 58; j++)
+		for (j = 48; j <= 57; j++)
 		{
 			l = j + 1;
 			for (k = i; k <= 57; k++)
@@ -36,6 +36,8 @@ int main(void)
 				}
 				l = 48;
 			}
+			if (i == 57 && j == 57)
+				return (0);
 		}
 	}
 	putchar('\n');

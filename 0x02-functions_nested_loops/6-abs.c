d5bf18e46b88d30c6e2558f797dcdfebdb8467c7
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-14 14:31:57 -0700

    absolute vlaue

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <sdtlib.h>
+/**
+ *_abs - get the absolute value of a number
+ *
+ *@c: the number
+ *
+ * Return: the absolute value of c
+ */
+int _abs(int c)
+{
+	return (abs(c));
+}

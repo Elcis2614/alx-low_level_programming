0b2345d49f33999f668e66bb2a591977b85a2501
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-14 16:43:21 -0700

    table of nine

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,16 +10,27 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar('0');
+		_putchar(',');
+		for (j = 1; j <= 9; j++)
 		{
 			x = j * i;
-			_putchar('0' + x);
+			_putchar(' ');
+			if (x >= 10)
+			{
+				_putchar(x / 10 + '0');
+				_putchar(x % 10 + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar('0' + x);
+			}
 			if (j == 9)
 				_putchar('\n');
 			else
 			{
 				_putchar(',');
-				_putchar(' ');
 			}
 		}
 	}

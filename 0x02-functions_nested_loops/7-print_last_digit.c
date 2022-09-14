c2072c7e0bb12ea769600592a77a5730447f24f3
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-14 14:42:05 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *print_last_digit - prints the alphabet in lower case
+ *
+ *@c: the number
+ *
+ * Return: last digit of a number
+*/
+int print_last_digit(int c)
+{
+	return (c % 10);
+}

61d959c65357146ea32e9751fad2f13713f8768b
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-14 14:19:50 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ *print_sign - checks and print the sign of number
+ *
+ *@n: the number to be tested
+ *Return: 0,1,-1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar ('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar (0);
+		return (0);
+	}
+}

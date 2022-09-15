e4adad0c2440b9c56630738fac69905aa2df1916
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-14 18:12:42 -0700

    n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,20 +13,18 @@ void print_to_98(int n)
 	if (n <= 98)
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d", i);
 			if (i == 98)
-				printf("%s\n");
+				printf("%d\n", i);
 			else
-				printf(", ");
+				printf("%d\n, ", i);
 		}
 	else
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d", i);
 			if (i == 98)
-				printf("%s\n");
+				printf("%d\n", i);
 			else
-				printf(", ");
+				printf("%d ,", i);
 		}
 
 }

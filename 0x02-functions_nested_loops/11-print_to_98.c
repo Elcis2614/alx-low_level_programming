7305f14fdc5b0ff8d301c22bad7176044c410e96
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-14 17:28:22 -0700

    to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *print_to_98 - print numbers to 98  in range n
+ *
+ * @n: range
+ * Return: 0
+*/
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+		for (i = n; i <= 98; i++)
+		{
+			printf(i);
+			if (i == 98)
+				printf('\n');
+			else
+				printf(", ");
+		}
+	else
+		for (i = n; i >= 98; i--)
+		{
+			printf(i);
+			if (i == 98)
+				printf('\n');
+			else
+				printf(", ");
+		}
+
+}

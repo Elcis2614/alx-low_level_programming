d114b66f1450861f12e24b2dce07f5df04e8f60c
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-15 14:37:29 -0700

    0x03-debugging/

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,24 @@
+#include <main.h>
+
+/**
+ *largest_number - return the largest of three numbers
+ *@a: number 1
+ *@b: number 2
+ *@c: number 3
+ * Return: the largest
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int larger;
+
+	if (a >= b && a >= c)
+		larger = a;
+	else if (b >= a && b >= c)
+		larger = b;
+	else
+		larger = c;
+	return (larger);
+
+
+}

c44bd7b7d472b7499c5c02a7a32a916cc3a97365
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-16 20:14:43 -0700

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_triangle - prints the diagonal`
+ *@size: deffines the length of the line
+ */
+void print_triangle(int size)
+{
+	int i, j, x;
+
+	x = 1;
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+		while (size >= 0)
+		{
+			for (j = 0; j < size - 1; j++)
+			{
+				_putchar(' ');
+			}
+			for (i = 0; i < x; i++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+			size--;
+			x++;
+		};
+}

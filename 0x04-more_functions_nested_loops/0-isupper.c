512de51803a59dcf049d02dcb2d4aff5ba0affef
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-16 17:19:35 -0700

    more on loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include <ctype.h>
+/*
+ *_isupper - check it a character is upper case
+ *@c: the character to be tested
+ *Return: 1 if true , 0 otherwise
+ */
+int _isupper(int c)
+{
+	if(isupper(c))
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+The is file is more about nested loop

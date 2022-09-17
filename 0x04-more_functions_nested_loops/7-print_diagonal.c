4af16d20a4f6bcf5bb48a7ef1c7f1cbf9e0c674a
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-16 19:52:13 -0700

    diagonal print

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,14 +13,14 @@ void print_diagonal(int n)
 		_putchar('\n');
 		return;
 	}
-
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < i; i++)
+	else
+		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
-		}
-		_putchar('\\');
-		_putchar('\n');
-	}
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		};
 }

12ba861ecc992d4431228d49168956e549e7851b
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-21 13:20:41 -0700

    cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
- *string_toupper - convert all characters of string to Upper case
+ *cap_string - capitaize all words
  *
  *@a: the array
  *Return: a point to string
  */
-char *string_toupper(char *a)
+char *cap_string(char *a)
 {
 	int i, n;
 
 	n = strlen(a);
 	a[0] = toupper(a[0]);
-	for (i = 1; i < n; i++)
+	for (i = 1; i < n - 1; i++)
 	{
-		if (isalnum(a[i]) == 0 && a[i + 1] != '\0')
-			a[i + 1] = isupper(a[i + 1]);
+		if (isalnum(a[i]) == 0)
+			a[i + 1] = toupper(a[i + 1]);
 	}
 	return (a);
 }

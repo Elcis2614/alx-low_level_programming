3f92010589141148a0efe5cb5e055c81698bd45a
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-20 18:20:17 -0700

    more reversing

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ *rev_string -  prints a string to the stout in reverse order
+ *
+ * @s: the string to be printed
+ * source : https://stackoverflow.com/a/72097452
+ */
+void rev_string(char *s)
+{
+	size_t n;
+	char *c;
+
+	n = strlen(s);
+	c = malloc(n + 1);
+	if (c != NULL)
+	{
+		c += n;
+		*c = '\0';
+		while (*s)
+		{
+			*--c = *s++;
+		}
+	}
+	puts(c);
+}

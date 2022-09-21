e7dd729a0de12286dfe93d8b17eb23e2f3e48699
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-21 14:16:51 -0700

    encoding

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *leet - encodes
+ *
+ *@a: the array
+ *Return: a point to string
+ */
+char *leet(char *a)
+{
+	int i, n;
+
+	n = strlen(a);
+	for (i = 0; i < n; i++)
+	{
+		if (tolower(a[i]) == 'a')
+			a[i] = '4';
+		else if (tolower(a[i]) == 'e')
+			a[i] = '3';
+		else if (tolower(a[i]) == 'o')
+			a[i] = '0';
+		else if (tolower(a[i]) == 't')
+			a[i] = '7';
+		else if (tolower(a[i]) == 'l')
+			a[i] = '1';
+		else
+			continue;
+	}
+	return (a);
+}

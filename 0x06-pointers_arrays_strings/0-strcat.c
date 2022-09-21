4e84304386fa515e4bf621c74e573bb8ee4b2b0e
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-21 09:44:31 -0700

    more on pointers

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+/**
+ *_strcat - concatenate won strings
+ *
+ * @dest: destination string
+ * @src: source string
+ *Return: returns a pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}

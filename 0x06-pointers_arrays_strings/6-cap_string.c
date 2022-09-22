028e8dc40dcfc7b150ef2dc77ed3e8e9da10ccf6
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-22 09:50:25 -0700

    keep on trying

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,10 +13,8 @@ char *cap_string(char *a)
 	a[0] = toupper(a[0]);
 	for (i = 1; i < n - 1; i++)
 	{
-		if (isalnum(a[i]) == 0 && isalpha(a[i + 1] != 0))
-			a[i + 1] = toupper(a[i + 1]);
+		if (isalnum(a[i - 1]) == 0 && isalpha(a[i + 1]) != 0)
+			a[i] = toupper(a[i]);
 	}
-	if (isalnum(a[n - 1]) != 0 && isalnum(a[n - 2]))
-		a[n - 1] = toupper(a[n - 1]);
 	return (a);
 }

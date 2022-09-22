1c33c7120732c64022a9c536b440a5449f5304d6
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-22 08:55:06 -0700

    capitalizing

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,7 @@ char *cap_string(char *a)
 	a[0] = toupper(a[0]);
 	for (i = 1; i < n - 1; i++)
 	{
-		if (isalnum(a[i]) == 0 && isalpha(a[i + 1]) != 0)
+		if (isalnum(a[i]) == 0)
 			a[i + 1] = toupper(a[i + 1]);
 	}
 	return (a);

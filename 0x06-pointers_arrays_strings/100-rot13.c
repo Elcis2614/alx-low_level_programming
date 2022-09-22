d675ded2ae61d1a9028f1235e154b7803251d755
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-22 14:24:19 -0700

    encoding tricky

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,21 +7,33 @@
  */
 char *rot13(char *a)
 {
-	int i, n, j;
-	char l[5] = {'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g',
+	int i, n;
+	char l[26] = {'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g',
 		'G', 'h', 'H', 'i', 'I', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M'};
 
-	char c[5] = {'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't',
+	char c[26] = {'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't',
 		'T', 'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z'};
 
 	n = strlen(a);
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < 26; j++)
+		int k = 0;
+
+		while (k < 26 && a[i] != l[k] && isalpha(a[i]) != 0)
+			++k;
+		while (k >= 26 && a[i] != c[k - 26] && isalpha(a[i]) != 0 && k < 52)
+			++k;
+		if (k < 26 && isalpha(a[i]) != 0)
 		{
-			if (a[i] == l[j])
-				a[i] = c[j];
+			a[i] = c[k];
+			continue;
 		}
+		else 
+			while (k !=0)
+			{
+				a[i] = l[k - 26];
+				break;
+			}
 	}
 	return (a);
 }

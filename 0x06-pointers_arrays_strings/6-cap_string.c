e5cfdff6c1ad35ef2ad6dd4205f32ddfdb32e5e2
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-22 09:22:45 -0700

    still capitizing

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,8 +13,10 @@ char *cap_string(char *a)
 	a[0] = toupper(a[0]);
 	for (i = 1; i < n - 1; i++)
 	{
-		if (isalnum(a[i]) == 0)
+		if (isalnum(a[i]) == 0 && a[i] != '\t')
 			a[i + 1] = toupper(a[i + 1]);
+		else if(a[i] == '\t')
+			a[i] = ' ';
 	}
 	if (isalnum(a[n - 1]) != 0 && isalnum(a[n - 2]))
 		a[n - 1] = toupper(a[n - 1]);

5c7a244842234a340bc86ec1f0166f3425dfecf2
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-22 11:04:49 -0700

    encoding rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *rot13 - encodes using rot13
+ *
+ *@a: the array
+ *Return: a point to string
+ */
+char *rot13(char *a)
+{
+	int i, n, j;
+	char l[5] = {'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g',
+		'G', 'h', 'H', 'i', 'I', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M'};
+
+	char c[5] = {'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't',
+		'T', 'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z'};
+
+	n = strlen(a);
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (tolower(a[i]) == l[j])
+				a[i] = c[j];
+		}
+	}
+	return (a);
+}

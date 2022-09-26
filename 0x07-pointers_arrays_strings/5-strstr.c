7430090ecb4396d5bc3647d07aead980b344347f
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-26 13:43:13 -0700

    more string functions

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *_strstr - finds the first occurence a string in another
+ *@haystack: the string 1
+ *@needle: the character string being searching
+ *Return: the pointer to the first occurence of accept of null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}

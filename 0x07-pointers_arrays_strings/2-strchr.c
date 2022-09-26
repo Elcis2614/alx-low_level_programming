2bafd63f6cd9f53978ed6d9964a05bf889ed3e8a
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-26 10:39:18 -0700

    searching a char in str

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *_strchr - finds the first occrence of a character into a string
+ *@s: the string
+ *@c: the character
+ *Return: the pointer to the first occurence of c of null
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}

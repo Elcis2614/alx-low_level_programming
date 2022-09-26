cb3984ccbfd14880782f9015595ef47f07c9cc5c
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-26 10:18:33 -0700

    more string functions

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_memcpy - function copies n bytes from memory area src to memory area dest
+ *@dest: the destination
+ *@src: the source
+ *@n: the number of bytes
+ *Return: the pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+}
+	memcpy(dest, src, n);
+	return (dest);
+}

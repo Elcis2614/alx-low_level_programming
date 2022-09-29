c3e38d2534ce47737ff2b4f3b0670c2f316dcc6a
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-29 10:32:43 -0700

    correction

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,12 +6,11 @@
 void _print_rev_recursion(char *s)
 {
 	size_t t = strlen(s);
+	char *x;
 
 	if (t == 0)
 		return;
 	_putchar(s[t - 1]);
-	char *x;
-
 	x = malloc(t - 1);
 	memcpy(x, s, t - 1);
 	_print_rev_recursion(x);

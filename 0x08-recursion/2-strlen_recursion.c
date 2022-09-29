cc8095ea3d784462d1b2a7c6ab3c66ef17aaf447
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-09-29 14:34:41 -0700

    lenght

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *_strlen_recursion - return the lenght of the string
+ *@s: the pointer
+ *Return: the length
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}

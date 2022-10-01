0905a55e085509b6250f0a6a49995470ccb4ba0a
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-10-01 14:14:02 -0700

    dammmm

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,36 @@
 #include "main.h"
 /**
+ *s - finds the square of a number of the boundaries
+ *@low: lower boundery
+ *@high: higher
+ *@N: the number
+ *Return: the highest root
+ */
+int s(int low, int high, int N)
+{
+	/* If the range is still valid*/
+	if (low <= high)
+	{
+		/* Find the mid-value of the range*/
+		int mid = (low + high) / 2;
+
+		if ((mid * mid <= N) && ((mid + 1) * (mid + 1) > N))
+		{
+			return (mid);
+		}
+		/* Condition to check if left search space is useless*/
+		else if (mid * mid < N)
+		{
+			return (s(mid + 1, high, N));
+		}
+		else
+		{
+			return (s(low, mid - 1, N));
+		}
+	}
+	return (low);
+}
+/**
  *_sqrt_recursion - calculates the sqrt of a number
  *@n: the number
  *Return: the sqaure of that number or -1 ifit doesnt exist
@@ -10,9 +41,10 @@ int _sqrt_recursion(int n)
 
 	if (n >= 0)
 	{
-		x = (int)pow(n, 0.5);
-		if (n * n != x)
-			x = -1;
+		int t = s(0, n, n);
+
+		if (t * t == n)
+			x = t;
 	}
 	return (x);
 }

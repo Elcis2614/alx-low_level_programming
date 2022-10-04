a344c8057b0a9e2a9238818868958aee52b0cae5
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-10-04 12:02:12 -0700

    concatenating

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * str_concat -  concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: the pointer to the complety string
+ */
+char *str_concat(char *s1, char *s2);
+{
+	int x, y, i;
+	char *s;
+
+	x = strlen(s1);
+	y = strlen(s2);
+	s = malloc(sizeof(char) * (x + y));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < x; i++)
+		s[i] = s1[i];
+	for (i = x; i < (x + y); i++)
+		s[i] = s2[i - x];
+	return (s);
+}

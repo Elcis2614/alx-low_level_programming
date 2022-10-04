11fe4ef9e71f988e103b42ddbc53df421788f54c
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-10-04 11:14:06 -0700

    let go malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * create_array - creates of chars and initialize it
+ * @size: the size of the array
+ * @c: the character to fill the array with
+ * Return: the pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+		return (NULL);
+	int i;
+	char *array;
+
+	array = (char *) malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <math.h>
+int _putchar(char);
+char *create_array(unsigned int size, char c);
+#endif /* MAIN_H */

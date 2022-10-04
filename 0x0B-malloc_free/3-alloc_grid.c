87181d59472fa20ed8d9eb856b153294ac9292c6
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-10-04 13:04:27 -0700

    more on grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,14 +15,14 @@ int **alloc_grid(int width, int height)
 	array = (int **) malloc(height * sizeof(int *));
 	if (array == NULL)
 		return (NULL);
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
 		array[i] = (int *) malloc(sizeof(int) * width);
 		if (array[i] == NULL)
 			return (NULL);
 	}
-	for (i = 0; i < width; i++)
-		for (j = 0; j < height; j++)
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
 			array[i][j] = 0;
 	return (array);
 }

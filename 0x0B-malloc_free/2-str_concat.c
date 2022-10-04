ca40b71b29a6a0bd56cd9db193faa2384882be07
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-10-04 12:36:40 -0700

    more concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,18 +10,29 @@ char *str_concat(char *s1, char *s2)
 	int x, y, i;
 	char *s;
 
-	if (s1 == NULL)
-		s1 = "";
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
 	else if (s2 == NULL)
-		s2 = "";
-	x = strlen(s1);
-	y = strlen(s2);
+	{
+		y = 0;
+		x = strlen(s1);
+	}
+	else if (s1 == NULL)
+	{
+		x = 0;
+		y = strlen(s2);
+	}
+	else
+	{
+		x = strlen(s1);
+		y = strlen(s2);
+	}
 	s = malloc(sizeof(char) * (x + y + 1));
 	if (s == NULL)
 		return (NULL);
-	for (i = 0; i < x; i++)
+	while (i  < x && x != 0)
 		s[i] = s1[i];
-	for (i = x; i < (x + y); i++)
+	while (i < (x + y) && y != 0)
 		s[i] = s2[i - x];
 	return (s);
 }

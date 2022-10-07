8b88498aa7fad22c79d6f21a4c8d4280343da8e7
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-10-07 12:35:51 -0700

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *_calloc - allocates memory to a array
+ *@nmemb: size of array
+ *@size: size of each element
+ *Return: the adress on success
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+
+	int *c = (int *) malloc(nmemb *size);
+
+	if (c == NULL)
+	{
+		return (NULL);
+	}
+	else if (nmemb == 0 || size == 0)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+	{
+		*(c + i) = 0;
+	}
+	return (c)
+
+}

1205c963a65c85d941c6aae1ed09a17c77ba2636
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-10-12 16:00:02 -0700

    new dog in town

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "dog.h"
 /**
  * new_dog - creates a dog_t object
@@ -11,12 +9,12 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	
+
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	dog->name = *name;
-	dog->owner = *owner;
+	dog->name = name;
+	dog->owner = owner;
 	dog->age = age;
 	return (dog);
 }

dc4354512431f90e97c60ac712ec6db893c144e6
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-10-12 10:55:19 -0700

    adding the dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,11 +3,16 @@
 #define DOG_H
 
 #include "main.h"
+/**
+ *struct dog - structure
+ *@name: name of the dog
+ *@age:age of the dog
+ *@owner: owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-typef struct dog dog_t;
 #endif

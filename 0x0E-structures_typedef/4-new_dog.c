88a0f5151f4090e3e0704400a293676d438bb27b
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-10-14 05:08:19 -0700

    new dog again '

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,11 +16,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (name != NULL)
 		dog->name = name;
 	else if (name == NULL)
-		dog->name = "";
+		return (NULL);
 	if (owner != NULL)
 		dog->owner = owner;
 	else if (owner == NULL)
-		dog->owner = "";
+		return (NULL);
 	dog->age = age;
 	return (dog);
 }

9b14989f103d531fbb786b13a3760bcf624e8c8a
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-10-14 20:31:35 -0700

    strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,18 +10,22 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
+	char *s;
 
 	if (n == 0)
 		return;
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
+		char *s = va_arg(ap, char*);
+		if (s == NULL)
+			continue;
 		if (i == n - 1 || separator == NULL)
 		{
-			printf("%s", va_arg(ap, char *));
+			printf("%s", s);
 			continue;
 		}
-		printf("%s%s", va_arg(ap, char *), separator);
+		printf("%s%s", s, separator);
 	}
 	printf("\n");
 	va_end(ap);

792bfb6984da2b2c3ef980e32e79db4d3df24360
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-10-14 18:51:52 -0700

    correction 2

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,6 +14,7 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 	sum = 0;
+	va_start(ap, n);
 	for (i = 0; i < n; i++)
 		sum += (int) va_arg(ap, const unsigned int);
 	va_end(ap);

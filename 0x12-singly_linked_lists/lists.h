d2163ae8bf0fac061fc9b7f1f3a97ae7c43e4e5f
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-10-24 11:52:32 -0700

    lists.h betty

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,13 +3,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+typedef struct list_t list_t;
 /**
  * struct list_t - linked list
  * @str: string part
- * @len: lenght of the string
+ * @len: the lenght of the string
  * @next: address of the next element
  */
-typedef struct list_t list_t;
 struct list_t
 {
 	char *str;

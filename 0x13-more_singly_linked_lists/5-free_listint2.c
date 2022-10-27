aadbee3b78a15e00e1a391e94a59bb57cd587902
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2022-10-27 15:50:50 -0700

    5teaasks

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,17 +7,21 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tail, *h;
 
-	if (head != NULL && *head != NULL)
+	if (head != NULL)
 	{
-		tail = *head;
-		while (tail->next != NULL)
+		if (*head != NULL)
 		{
-			h = tail;
-			tail = tail->next;
-			free(h);
+			tail = *head;
+			while (tail->next != NULL)
+			{
+				h = tail;
+				tail = tail->next;
+				free(h);
+			}
+			free(tail);
 		}
-		free(tail);
+		*head = NULL;
+		head = NULL;
+
 	}
-	*head = NULL;
-	head = NULL;
 }

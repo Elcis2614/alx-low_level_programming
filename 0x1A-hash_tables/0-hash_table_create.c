a62a2355b9dc9c62744dcf91e0c0cc88319fc2bd
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2023-01-14 07:12:18 -0800

    hash tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates the has table
+ * @size: the size of the table
+ * Return: the pointer to the table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return NULL;
+	table->size = size;
+	table->array = malloc(size * sizeof(hash_node_t*));
+	if(table->array == NULL)
+	{
+		free(table);
+		return NULL;
+	}
+	return table;
+}

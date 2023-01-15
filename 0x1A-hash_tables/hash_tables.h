495fff57fea86b393e59c701fd06dc743b547b28
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2023-01-15 06:32:21 -0800

    finding the index

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,9 +1,7 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
-
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
  * struct hash_node_s - Node of a hash tabl
  * @key: The key, string
@@ -29,6 +27,9 @@ typedef struct hash_table_s
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
+
 unsigned long int hash_djb2(const unsigned char *str);
+
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 #endif

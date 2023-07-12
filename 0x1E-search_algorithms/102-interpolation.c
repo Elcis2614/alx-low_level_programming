12b8bb78976d668d09f8c3c40efb579fc1f4c4fb
elcis2614 <elisereflorbusole@gmail.com>
2023-07-12 20:16:55 +0000

    interpolation algorith

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,50 @@
+#include "search_algos.h"
+/**
+ * interpolation_search - searches value in sorted array of int using the Interpolation search
+ * @array: the pointer to the array being searched in
+ * @size: the (int)size of the array
+ * @value: the key being search for
+ * Return: the index of value if found, or -1 otherwise
+*/
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t low, high, pos;
+
+	if (array != NULL && size > 0)
+	{
+		low = 0;
+		high = size - 1;
+		pos = position(array, high, low, value);
+
+		if (value > array[high])
+		{
+			printf("Value checked array[%lu] is out of range\n", pos);
+			return (-1);
+		}
+
+                while (low <= high)
+                {
+			printf("Value checked array[%i] = [%i]\n", (int)pos, array[pos]);
+                        if (value == array[pos])
+                                return (int)(pos);
+
+                        else if (value < array[pos])
+                                high = pos - 1;
+
+                        else
+                                low = pos + 1;
+                        pos = position(array, high, low, value);
+                }
+	}
+	return (-1);
+}
+/**
+ * position - computes the probe position 
+ * @high : the high index
+ * @low: the low index
+ * @value : the value
+ */
+size_t position(int *array, size_t high, size_t low, int value)
+{
+	return (low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low])));
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,4 +6,6 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void printArray(int *array, int high, int low);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+size_t position(int *array, size_t high, size_t low, int value);
 #endif
